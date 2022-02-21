66b0e222d46fed8dae3642b99cc614f56e00bc02
Pablo Fernandez <fernandezpablo85@gmail.com>
2022-02-21 20:51:02 -0300

    exercise statement

---
+++ episodes/v1.0/run-length-encoding/src/main.ts
@@ -0,0 +1,9 @@
+type RunLengthEncodedString = [string, number][];
+
+// given a string, return each repeating character and it's run count, for example:
+// "aaaabbbbccc" => [["a", 4], ["b", 3], ["c", 3]]
+//
+// see https://en.wikipedia.org/wiki/Run-length_encoding
+export function rle(plain: String): RunLengthEncodedString {
+  return [];
+}
---
+++ episodes/v1.0/run-length-encoding/test/main.test.ts
@@ -0,0 +1,40 @@
+import fc from "fast-check";
+import { rle } from "../src/main";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(1 + 2).toBe(3);
+});
+
+test("rle for empty strings should be the empty array", () => {
+  expect(rle("")).toStrictEqual([]);
+});
+
+test("rle single letter should be the letter with count of 1", () => {
+  expect(rle("a")).toEqual([["a", 1]]);
+});
+
+test("rle of 10 letters should be the letter with count of 10", () => {
+  expect(rle("aaaaaaaaaa")).toEqual([["a", 10]]);
+});
+
+test("rle letters should be the letters with their count", () => {
+  expect(rle("abbcccdddd")).toEqual([
+    ["a", 1],
+    ["b", 2],
+    ["c", 3],
+    ["d", 4],
+  ]);
+});
+
+// this is just an example of a property-based-test
+it("sum is commutative", () => {
+  const commutativity = fc.property(
+    fc.integer(),
+    fc.float(),
+    (a: number, b: number) => {
+      return a + b === b + a;
+    }
+  );
+
+  fc.assert(commutativity, { verbose: true });
+});
