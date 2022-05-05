b71eb07f1a67b89d387d9b2df49a0bfdc49aa643
Pablo Fernandez <fernandezpablo85@gmail.com>
2022-05-04 23:20:07 -0300

    remove notes

--- episodes/v1.1/src/rounding_property_test.py
+++ episodes/v1.1/src/rounding_property_test.py
@@ -8,21 +8,28 @@ def noround(portfolio):
     return {k: v / sum(portfolio.values()) * 100 for k, v in portfolio.items()}
 
 
-@settings(max_examples=1000)
+examples = 1_000
+
+
+@settings(max_examples=examples)
 @given(portfolios())
 def test_rounding_should_add_up_to_100(portfolio):
-    note(portfolio)
     pcts = percentages(portfolio)
-    note(pcts)
     total = sum(pcts.values())
     assert total == 100
 
 
-@settings(max_examples=1000)
+@settings(max_examples=examples)
 @given(portfolios())
 def test_percentages_should_be_all_positive(portfolio):
-    note(noround(portfolio))
     pcts = percentages(portfolio)
-    note(pcts)
+    is_positive = [v > 0 for _, v in pcts.items()]
+    assert all(is_positive)
+
+
+@settings(max_examples=examples)
+@given(portfolios())
+def test_percentages_should_be_all_positive(portfolio):
+    pcts = percentages(portfolio)
     is_positive = [v > 0 for _, v in pcts.items()]
     assert all(is_positive)
